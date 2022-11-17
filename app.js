d392331ae46dd2fad15bba70defab0a3375bcd00
KLAIIIII <guyyg03546@gmail.com>
2022-11-17 10:47:54 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+// 1.assignment variable
 const fname = "Mark Zuckaberg"
 
+// 2.Function
+function Hello(){
+    return (
+        <div>
+            <h1 className="text">Welcome to ReactJS!</h1>
+            <hr />
+            <h1 className="text2">Hello, World! {fname}</h1>
+        </div>
+    )
+}
+
+function MyApp(){
+    return(
+        <div>
+
+        </div>
+    )
+}
 
 
-const domContainer = document.querySelector('#root');
-const root = ReactDOM.createRoot(domContainer);
-root.render(fname);
+// const domContainer = document.querySelector('root');
+// const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Hello />);
